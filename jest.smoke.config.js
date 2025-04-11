432b07c9e7be0c2777155ca3e2d2167840c62c9b
vibewell25 <vibewellltd@gmail.com>
2025-04-11 03:27:43 +0100

    Update package configuration and dependencies

--- jest.smoke.config.js
+++ jest.smoke.config.js
@@ -1,5 +1,17 @@
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/tests/smoke/**/*.test.ts'],
+  testMatch: ['**/tests/smoke/**/*.ts', '**/tests/rate-limiting/**/*.ts'],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', {
+      tsconfig: 'tsconfig.json'
+    }],
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  setupFilesAfterEnv: ['./jest.node.setup.js'],
+  testTimeout: 10000,
+  transformIgnorePatterns: [
+    '/node_modules/(?!(@supabase|jose|msw|@mswjs)/)'
+  ]
 }; 

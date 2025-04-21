57e5762fac5e4eccbaf319e6d8f7cf0e553a25c8
vibewell25 <vibewellltd@gmail.com>
2025-04-21 22:19:40 +0100

    chore: update gitignore and remove node_modules from tracking

--- __mocks__/web3modal/dist/providers/connectors.js
+++
@@ -1,6 +0,0 @@
-// Mock for coinbaseWallet connector
-const coinbaseWallet = jest.fn().mockImplementation(() => ({}));
-
-module.exports = {
-  coinbaseWallet
-}; 
--- mobile/node_modules/expo-calendar/plugin/build/withCalendar.d.ts
+++
@@ -1,6 +0,0 @@
-import { ConfigPlugin } from 'expo/config-plugins';
-declare const _default: ConfigPlugin<void | {
-    calendarPermission?: string | false | undefined;
-    remindersPermission?: string | false | undefined;
-}>;
-export default _default;

499f082359e759c7a0e80268464328d33877e2bf
M B <mb@Ms-MacBook-Pro.local>
2025-05-04 18:45:00 +0100

    fix: update React Native type declarations to properly extend Component class

--- apps/mobile/src/types/react-native.d.ts
+++ apps/mobile/src/types/react-native.d.ts
@@ -4,22 +4,49 @@ import type { ComponentType, Component } from 'react';
 declare module 'react-native' {
   interface ViewProps {
     ref?: any;
+    style?: any;
   }
   
   interface TextProps {
     ref?: any;
+    style?: any;
   }
   
   interface FlatListProps<ItemT> {
     ref?: any;
+    data?: ItemT[];
+    renderItem?: (info: { item: ItemT; index: number }) => React.ReactElement | null;
+    keyExtractor?: (item: ItemT, index: number) => string;
+    contentContainerStyle?: any;
+    showsVerticalScrollIndicator?: boolean;
   }
   
   interface SafeAreaViewProps {
     ref?: any;
+    style?: any;
   }
 
-  export class View extends Component<ViewProps> {}
-  export class Text extends Component<TextProps> {}
-  export class FlatList<ItemT = any> extends Component<FlatListProps<ItemT>> {}
-  export class SafeAreaView extends Component<SafeAreaViewProps> {}
+  export class View extends Component<ViewProps> {
+    refs: {
+      [key: string]: any;
+    };
+  }
+  
+  export class Text extends Component<TextProps> {
+    refs: {
+      [key: string]: any;
+    };
+  }
+  
+  export class FlatList<ItemT = any> extends Component<FlatListProps<ItemT>> {
+    refs: {
+      [key: string]: any;
+    };
+  }
+  
+  export class SafeAreaView extends Component<SafeAreaViewProps> {
+    refs: {
+      [key: string]: any;
+    };
+  }
 } 
